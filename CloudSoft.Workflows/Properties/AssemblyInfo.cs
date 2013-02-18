2c769b5f91923c259f33f72906fb6987f760058a
Marc Chouteau <chouteau@gmail.com>
2013-02-18 22:26:36 +0100

    Change Assembly Info

--- CloudSoft.Workflows/Properties/AssemblyInfo.cs
+++ CloudSoft.Workflows/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ERPStore.Workflows")]
+[assembly: AssemblyTitle("CloudSoft.Workflows")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ERPStore.Workflows")]
+[assembly: AssemblyCompany("CloudSoft")]
+[assembly: AssemblyProduct("CloudSoft.Workflows")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
